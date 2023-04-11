0ea767ab20ef156a2399627a94950c73c3a3e967
ExponentialWorkload <90570076+Exponential-Workload@users.noreply.github.com>
2023-04-11 03:00:08 +0200

    feat: Add blacklisted props file

---
+++ BlacklistedProps.lua
@@ -0,0 +1,35 @@
+return {
+  ['TextLabel'] = {
+    'Transparency', -- Mix of BackgroundTransparency & TextTransparency
+    'Font', -- superseeded by FontFamily
+  },
+  ['TextButton'] = {
+    'Transparency', -- Mix of BackgroundTransparency & TextTransparency
+    'Font', -- superseeded by FontFamily
+  },
+  ['TextBox'] = {
+    'Transparency', -- Mix of BackgroundTransparency & TextTransparency
+    'Font', -- superseeded by FontFamily
+  },
+  ['GuiBase'] = {
+    'Transparency',
+  },
+  ['GuiBase2d'] = {
+    'Transparency',
+  },
+  ['GuiBase3d'] = {
+    'Transparency',
+  },
+  ['UIStroke'] = {
+    'Transparency',
+  },
+  ['UIGradient'] = {
+    'Transparency',
+  },
+  ['BasePart'] = {
+    'BrickColor',
+  },
+  ['Part'] = {
+    'BrickColor',
+  },
+}
